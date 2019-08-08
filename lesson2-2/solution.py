2ccd491de60f2bcc1f73615127d246ebe6ca01e3
YMyajima <buiiudymi.yajima@gmail.com>
2019-08-08 21:49:58 +0900

    solvel lesson 2-2

--- lesson2-2/solution.py
+++ lesson2-2/solution.py
@@ -1,7 +1,8 @@
 
 class TestValue:
-    def __init__(self, A, expect):
+    def __init__(self, A, K, expect):
         self.A = A
+        self.K = K
         self.expect = expect
 
 
@@ -9,34 +10,45 @@ class Solution:
 
     def __init__(self):
         self.test_values = [
-            TestValue([9, 3, 9, 3, 9, 7, 9], 7),
-            TestValue([1], 1)
+            TestValue([3, 8, 9, 7, 6], 3, [9, 7, 6, 3, 8]),
+            TestValue([0, 0, 0], 1, [0, 0, 0]),
+            TestValue([1, 2, 3, 4], 4, [1, 2, 3, 4]),
+            TestValue([], 0, []),
+            TestValue([1, 2, 3], 0, [1, 2, 3]),
+            TestValue([1, 1, 2, 3, 5], 42, [3, 5, 1, 1, 2])
         ]
 
-    def solution(self, A):
+    def solution(self, A, K):
         raise NotImplementedError("You must implement.")
 
     def test(self):
         for test_value in self.test_values:
-            s = self.solution(test_value.A)
+            s = self.solution(test_value.A, test_value.K)
             print(test_value.__dict__, 'result', s)
             assert s == test_value.expect
 
 
 class Solution1(Solution):
     """
-    OddOccurrencesInArray
+    CyclicRotation
     100%
-    O(N) or O(N*log(N))
     """
 
     def __init__(self):
         super().__init__()
 
-    def solution(self, A):
-        result = 0
-        for a in A:
-            result ^= a
+    def solution(self, A, K):
+        length = len(A)
+        if length == 0:
+            return A
+        rotate_count = K % length
+        if rotate_count == 0:
+            return A
+        result = [None] * len(A)
+        for i in range(length):
+            tmp = i + rotate_count
+            next_index = tmp % len(A)
+            result[next_index] = A[i]
         return result
 
 
